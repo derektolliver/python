9ed5a6a38c2d297fe298571cde7a3313cf9e63f5
Juan M Penaranda <jmpo1618@gmail.com>
2016-09-09 16:18:50 -0500

    Fixed screen size.

--- board.py
+++ board.py
@@ -2,7 +2,6 @@ from tkinter import *
 
 class Board(Frame):
 
-    def __init__(self, master, w, h):
-        frame = Frame(master)
-        Frame.__init__(self, frame, width = w, height = h, background = "white")
+    def __init__(self, w, h):
+        Frame.__init__(self, width = w, height = h, background = "white")
         #Frame.grid()
--- main.py
+++ main.py
@@ -3,6 +3,10 @@ from board import *
 
 if __name__ == "__main__":
     root = Tk()
+    width = root.winfo_screenwidth() // 2
+    height = int(root.winfo_screenheight() / 1.5)
+    size = str(width) + 'x' + str(height)
+    root.geometry(size)
     print(root.winfo_screenwidth())
-    board = Board(root, root.winfo_screenwidth(), root.winfo_screenheight())
+    board = Board(root.winfo_screenwidth(), root.winfo_screenheight())
     board.mainloop()
